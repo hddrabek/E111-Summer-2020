0187f5e9edeb390975fc2735f1991ff513527865
hddrabek <67345792+hddrabek@users.noreply.github.com>
2020-06-23 20:44:08 -0400

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main()
+
+{
+    int i=1;
+    while(i>0)
+    {
+        printf("Hello World\n");
+        sleep(3);
+    }
+    
+  return 0;
+  
+}
